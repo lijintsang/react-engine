56efea9d8d15117c807ac0935821e71e166ed9ba
lijinliang <lijl@qnzsai.com>
2024-12-11 17:14:32 +0800

    9 commit

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,19 +1,25 @@
-const webpack = require("webpack");
-const webpackMerge = require("webpack-merge");
-const baseConfig = require("./webpack.config");
-const TerserPlugin = require("terser-webpack-plugin");
+const webpack = require('webpack');
+const webpackMerge = require('webpack-merge');
+const baseConfig = require('./webpack.config');
+const TerserPlugin = require('terser-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 // const isProd = process.env.NODE_ENV === 'production';
 
 const config = {
-  mode: "production",
+  mode: 'production',
   cache: {
-    type: "filesystem",
+    type: 'filesystem',
     buildDependencies: {
       config: [__filename], //使用文件缓存
     },
   },
   optimization: {
+    // splitChunks 来分割代码，减少单个文件的大小
+    splitChunks: {
+      chunks: 'all',
+    },
     minimize: true, // 开启压缩
     minimizer: [
       new TerserPlugin({
@@ -35,8 +41,19 @@ const config = {
           // 利用多线程进行代码压缩，从而加速构建过程， 尤其是代码量较大时
         },
       }),
+      new CssMinimizerPlugin(),
     ],
   },
+  plugins: [
+    // 用于分析打包内容 http://127.0.0.1:8888/
+    // new BundleAnalyzerPlugin(),
+  ],
+  // performance: {
+  //   // 设置单文件最大限制为 512 KiB
+  //   maxAssetSize: 512000,
+  //   // 设置入口点资源总大小为 512 KiB
+  //   maxEntrypointSize: 512000,
+  // },
   devtool: 'source-map', // 启用 Source Map
 };
 
